b549c1abf711fa1684df783ee4baf75b029bfeea
Lauro Gripa <laurogripa@gmail.com>
2024-02-21 21:33:12 -0300

    Generate dist

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,36 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "CameraControls", {
+  enumerable: true,
+  get: function get() {
+    return _components.CameraControls;
+  }
+});
+Object.defineProperty(exports, "Lights", {
+  enumerable: true,
+  get: function get() {
+    return _components.Lights;
+  }
+});
+Object.defineProperty(exports, "Model", {
+  enumerable: true,
+  get: function get() {
+    return _components.Model;
+  }
+});
+Object.defineProperty(exports, "Particles", {
+  enumerable: true,
+  get: function get() {
+    return _components.Particles;
+  }
+});
+Object.defineProperty(exports, "Points", {
+  enumerable: true,
+  get: function get() {
+    return _components.Points;
+  }
+});
 Object.defineProperty(exports, "ThreeCanary", {
   enumerable: true,
   get: function get() {
@@ -12,7 +42,9 @@ Object.defineProperty(exports, "ThreeCanary", {
 Object.defineProperty(exports, "defaultConfig", {
   enumerable: true,
   get: function get() {
-    return _ThreeCanary.defaultConfig;
+    return _helpers.defaultConfig;
   }
 });
 var _ThreeCanary = require("./ThreeCanary");
+var _components = require("../components");
+var _helpers = require("../helpers");
